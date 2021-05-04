ac234c7c251a92f2977ccb42995a958b4d04ed4f
Ivy-0001 <69188068+Ivy-0001@users.noreply.github.com>
2021-05-04 13:39:34 +0400

    remove join perms checker
    remove join perms checker cuz its already in PlayerEvents/error

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -16,8 +16,7 @@ class Play extends Command {
     async run(message, args) {
         if (!message.member.voice.channel) return message.reply("❌ | You are not in a voice channel!");
         if (message.guild.me.voice.channel && message.guild.me.voice.channelID !== message.member.voice.channelID) return message.reply("❌ | You are not in my voice channel!");
-        if (!message.member.voice.channel.joinable) return message.reply("❌ | I don't have enough permission(s) to connect to your voice channel!");
-        if (!message.member.voice.channel.speakable) return message.reply("❌ | I don't have enough permission(s) to speak in your voice channel!");
+        if (message.member.voice.channel.joinable && !message.member.voice.channel.speakable) return message.reply("❌ | Oi mate, I am unable to speak in your voice channel, maybe I dont have enough permissions?");
         const query = args.join(" ");
         if (!query) return message.reply("❌ | Please include a search query!");
 
